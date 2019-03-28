8a7772709931d3d8dc1ccc8858a3c490fea5a61e
Alessio Bazzanella <alessio.bazzanella@me.com>
2019-03-28 16:12:43 +0100

    Split sending function

--- django_webix_sender/views.py
+++ django_webix_sender/views.py
@@ -16,10 +16,10 @@ from django.utils.translation import ugettext_lazy as _
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View, TemplateView
 
-from django_webix_sender.models import MessageSent, DjangoWebixSender
+from django_webix_sender.models import MessageSent
 from django_webix_sender.send_methods.skebby import SkebbyGateway
 from django_webix_sender.settings import CONF
-from django_webix_sender.utils import my_import, ISO_8859_1_limited
+from django_webix_sender.utils import send_mixin
 
 if apps.is_installed('filter'):
     from filter.models import Filter
@@ -146,148 +146,10 @@ class SenderSend(View):
         recipients = json.loads(request.POST.get("recipients", "{}"))
         presend = request.POST.get("presend", None)
 
-        # 1.a Recupero la lista delle istanze a cui inviare il messaggio (modello, lista destinatari)
-        _recipients_instance = []
-        for key, value in recipients.items():
-            app_label, model = key.lower().split(".")
-            model_class = apps.get_model(app_label=app_label, model_name=model)
-            if not issubclass(model_class, DjangoWebixSender):
-                raise Exception('{}.{} is not subclass of `DjangoWebixSender`'.format(app_label, model))
-            _recipients_instance += list(model_class.objects.filter(pk__in=value))
-        _recipients_instance = list(set(_recipients_instance))
+        result, status = send_mixin(send_method, typology, subject, body, recipients, presend,
+                                    user=request.user, files=request.FILES)
 
-        # 1.b Recupero i contatti collegati ai destinatari principali
-        for _recipient in _recipients_instance:
-            if hasattr(_recipient, 'get_sms_related'):
-                for related in _recipient.get_sms_related:
-                    if not issubclass(related.__class__, DjangoWebixSender):
-                        raise Exception(_('Related is not subclass of `DjangoWebixSender`'))
-                    _recipients_instance.append(related)
-        _recipients_instance = list(set(_recipients_instance))
-
-        # 2. Recupero la funzione per inviare
-        method, function = send_method.split(".", 1)
-        send_function = my_import(function)
-
-        # 3. Creo dizionario dei destinatari
-        _recipients = {
-            'valids': {
-                'recipients': [],
-                'address': []
-            },
-            'duplicates': {
-                'recipients': [],
-                'address': []
-            },
-            'invalids': []
-        }
-        if method == "sms":
-            for recipient in _recipients_instance:
-                # Contatto non ancora presente nella lista
-                if recipient.get_sms and not recipient.get_sms in _recipients['valids']['address']:
-                    _recipients['valids']['address'].append(recipient.get_sms)
-                    _recipients['valids']['recipients'].append(recipient)
-                # Contatto già presente nella lista (duplicato)
-                elif recipient.get_sms:
-                    _recipients['duplicates']['address'].append(recipient.get_sms)
-                    _recipients['duplicates']['recipients'].append(recipient)
-                # Indirizzo non presente
-                else:
-                    _recipients['invalids'].append(recipient)
-        elif method == "email":
-            for recipient in _recipients_instance:
-                # Contatto non ancora presente nella lista
-                if recipient.get_email and not recipient.get_email in _recipients['valids']['address']:
-                    _recipients['valids']['address'].append(recipient.get_email)
-                    _recipients['valids']['recipients'].append(recipient)
-                # Contatto già presente nella lista (duplicato)
-                elif recipient.get_email:
-                    _recipients['duplicates']['address'].append(recipient.get_email)
-                    _recipients['duplicates']['recipients'].append(recipient)
-                # Indirizzo non presente
-                else:
-                    _recipients['invalids'].append(recipient)
-
-        _recipients['valids'] = list(zip(_recipients['valids']['recipients'], _recipients['valids']['address']))
-        _recipients['duplicates'] = list(zip(_recipients['duplicates']['recipients'], _recipients['duplicates']['address']))
-
-        # 4 Verifica prima dell'invio (opzionale)
-        if presend is None:
-            if method == "sms":
-                # Verifico che il corpo dell'sms sia valido
-                invalid_characters = ''
-                for c in body:
-                    if c not in ISO_8859_1_limited:
-                        invalid_characters += c
-                if invalid_characters != '':
-                    return JsonResponse({
-                        'status': _('Invalid characters'),
-                        'data': invalid_characters
-                    }, status=400)
-            return JsonResponse({
-                'valids': len(_recipients['valids']),
-                'duplicates': len(_recipients['duplicates']),
-                'invalids': len(_recipients['invalids'])
-            })
-
-        # 5. Creo istanza `MessageAttachment` senza collegarlo alla m2m -> da collegare al passo 5
-        attachments = my_import(CONF['attachments']['save_function'])(
-            request.FILES,
-            send_method=send_method,
-            typology=typology,
-            subject=subject,
-            body=body,
-            recipients=_recipients
-        )
-
-        # 6. aggiungo il link del file in fondo al corpo
-        if len(attachments) > 0:
-            body += "\n\n"
-            for attachment in attachments:
-                body += "%s\n" % attachment.get_url()
-
-        # 7. Creo il log e collego gli allegati
-        # Costo del messaggio
-        _cost = 0
-        if hasattr(request.user, 'get_cost'):
-            _cost = request.user.get_cost(send_method)
-
-        # Mittente del messaggio
-        _sender = None
-        if hasattr(request.user, 'get_sender'):
-            _sender = request.user.get_sender()
-
-        message_sent = MessageSent(
-            send_method=send_method,
-            subject=subject,
-            body=body,
-            cost=_cost,
-            user=request.user,
-            sender=_sender
-        )
-        _extra = {}
-        if CONF['typology_model']['enabled']:
-            message_sent.typology_id = typology
-        message_sent.save()
-        message_sent.attachments.add(*attachments)
-
-        # 8. Send messages
-        if method == "sms":
-            # Invio i messaggi
-            result = send_function(_recipients, body, message_sent)
-            return JsonResponse({
-                'status': _('Sms sent'),
-                'extra': result.extra
-            })
-        elif method == "email":
-            # Invio i messaggi
-            result = send_function(_recipients, subject, body, message_sent)
-            return JsonResponse({
-                'status': _('Emails sent'),
-                'extra': result.extra
-            })
-        else:
-            return JsonResponse({'status': _('Invalid send method')}, status=400)
+        return JsonResponse(result, status=status)
 
 
 @method_decorator(login_required, name='dispatch')
